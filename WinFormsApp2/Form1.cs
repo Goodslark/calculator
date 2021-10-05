63c6fb7740ff10838b6f521d9766fd5afea1a9e3
Goodslark <Cmit6@yandex.ru>
2021-10-05 19:46:22 +0300

    add calculator

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Калькулятор : Form
+    {
+        public Калькулятор()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+
+            switch (comboBox1.Text)
+            {
+
+                case "+":
+                    textBox3.Text = Convert.ToString(a + b);
+                    break;
+
+                case "-":
+                    textBox3.Text = Convert.ToString(a - b);
+                    break;
+
+                case "*":
+                    textBox3.Text = Convert.ToString(a * b);
+                    break;
+
+                case "/":
+                    if (b==0)
+                    {
+                        MessageBox.Show("На ноль делить нельзя!"); 
+                    }
+                    textBox3.Text = Convert.ToString(a / b);
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            comboBox1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
